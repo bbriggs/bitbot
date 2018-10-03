e2cc7d73bfb0549106c6bdf7f919108676fc9419
Bren Briggs <bbriggs@users.noreply.github.com>
2018-10-03 11:34:17 -0400

    Trim spaces from incoming messages when matching for single-word comm… (#17)
    * Trim spaces from incoming messages when matching for single-word commands
    
    * Bump patch version

--- bitbot/info.go
+++ bitbot/info.go
@@ -2,12 +2,13 @@ package bitbot
 
 import (
 	"github.com/whyrusleeping/hellabot"
+	"strings"
 	"fmt"
 )
 
 var InfoTrigger = hbot.Trigger{
 	func(irc *hbot.Bot, m *hbot.Message) bool {
-		return m.Command == "PRIVMSG" && m.Content == "!info"
+		return m.Command == "PRIVMSG" && strings.TrimSpace(m.Content) == "!info"
 	},
 	func(irc *hbot.Bot, m *hbot.Message) bool {
 		resp := fmt.Sprintf("Bitbot version %s (%s/%s) | %s", GitVersion, GitBranch, GitCommit, SourceRepo)
