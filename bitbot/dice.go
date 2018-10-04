39b20b44c531b36acc968d14ee91a9c71d0250cb
Seth Beckman <beckmanseth@gmail.com>
2018-10-04 16:14:14 -0400

    Getting somewhere, maybe. Most of the code worked out (#18)

--- bitbot/dice.go
+++ bitbot/dice.go
@@ -4,14 +4,22 @@ import (
 	"github.com/whyrusleeping/hellabot"
 	"github.com/justinian/dice"
 	"strings"
-	"fmt"
+	
+	log "gopkg.in/inconshreveable/log15.v2"
 )
 var RollTrigger = hbot.Trigger{
 	func(irc *hbot.Bot, m *hbot.Message) bool {
 		return m.Command == "PRIVMSG" && strings.TrimSpace(m.Content) == "!roll"
 	},
 	func(irc *hbot.Bot, m *hbot.Message) bool {
-		
+		roll := strings.SplitAfter(m.Content, " ")
+		rollResult, blah, err := dice.Roll(roll[1])
+		if err != nil {
+			log.Error(err.Error())
+			return false  // keep processing triggers
+		}
+		irc.Reply(m, rollResult)
+		irc.Reply(m, blah)
 		return true
 	},
 }
