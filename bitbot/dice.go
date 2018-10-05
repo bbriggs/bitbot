9c35a75689c7721cd2c87e2f86d3ebf02deeb75a
Seth Beckman <p4rsec@users.noreply.github.com>
2018-10-05 17:06:16 -0400

    Adjusted code per changes requested in #19
    Signed-off-by: Seth Beckman <p4rsec@users.noreply.github.com>

--- bitbot/dice.go
+++ bitbot/dice.go
@@ -1,26 +1,34 @@
 package bitbot
 
 import (
-	"github.com/whyrusleeping/hellabot"
-	"github.com/justinian/dice"
-	"strings"
 	"fmt"
-	
+	"strings"
+
+	"github.com/justinian/dice"
+
 	log "gopkg.in/inconshreveable/log15.v2"
 )
+
 var RollTrigger = hbot.Trigger{
 	func(irc *hbot.Bot, m *hbot.Message) bool {
-		return m.Command == "PRIVMSG"
+		return m.Command == "PRIVMSG" && strings.TrimSpace(m.Content) == "!roll"
 	},
 	func(irc *hbot.Bot, m *hbot.Message) bool {
 		roll := strings.SplitAfter(m.Content, " ")
 		rollResult, _, err := dice.Roll(roll[1])
 		if err != nil {
 			log.Error(err.Error())
-			return false  // keep processing triggers
+			irc.Reply(m, "Usage: [num dice]d[sides](+/-num) (opt: if fudging)")
+			return false // keep processing triggers
 		}
-		resp := fmt.Sprintf("Roll: %s Result: %v", rollResult.Description(), rollResult.Int())
+		if roll[0] != "!roll" {
+			log.Error(err.Error())
+			irc.Reply(m, "You didn't call the command correctly.")
+			return false
+		}
+
+		resp := fmt.Sprintf("%v", rollResult.Int())
 		irc.Reply(m, resp)
-		return true
+		return false
 	},
 }
