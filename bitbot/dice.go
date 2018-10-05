08093d76a7d6ec7f946196d25f2e8174018cceaa
Seth Beckman <beckmanseth@gmail.com>
2018-10-05 11:42:47 -0400

    SHIT WORKS (#18)

--- bitbot/dice.go
+++ bitbot/dice.go
@@ -4,22 +4,23 @@ import (
 	"github.com/whyrusleeping/hellabot"
 	"github.com/justinian/dice"
 	"strings"
+	"fmt"
 	
 	log "gopkg.in/inconshreveable/log15.v2"
 )
 var RollTrigger = hbot.Trigger{
 	func(irc *hbot.Bot, m *hbot.Message) bool {
-		return m.Command == "PRIVMSG" && strings.TrimSpace(m.Content) == "!roll"
+		return m.Command == "PRIVMSG"
 	},
 	func(irc *hbot.Bot, m *hbot.Message) bool {
 		roll := strings.SplitAfter(m.Content, " ")
-		rollResult, blah, err := dice.Roll(roll[1])
+		rollResult, _, err := dice.Roll(roll[1])
 		if err != nil {
 			log.Error(err.Error())
 			return false  // keep processing triggers
 		}
-		irc.Reply(m, rollResult)
-		irc.Reply(m, blah)
+		resp := fmt.Sprintf("Roll: %s Result: %v", rollResult.Description(), rollResult.Int())
+		irc.Reply(m, resp)
 		return true
 	},
 }
