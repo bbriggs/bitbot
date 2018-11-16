0d514c8d9d35be81406ba3dccd924d718d5ac0fd
Seth Beckman <beckmanseth@gmail.com>
2018-11-15 21:36:11 -0500

    implemented patch you requested @bbriggs

--- bitbot/dice.go
+++ bitbot/dice.go
@@ -5,30 +5,36 @@ import (
 	"strings"
 
 	"github.com/justinian/dice"
-
+	"github.com/whyrusleeping/hellabot"
 	log "gopkg.in/inconshreveable/log15.v2"
 )
 
+const DICE_USAGE = "Usage: [num dice]d[sides](+/-num) (opt: if fudging)"
+
 var RollTrigger = hbot.Trigger{
 	func(irc *hbot.Bot, m *hbot.Message) bool {
-		return m.Command == "PRIVMSG" && strings.TrimSpace(m.Content) == "!roll"
+		return m.Command == "PRIVMSG" && strings.HasPrefix(m.Content, "!roll")
 	},
 	func(irc *hbot.Bot, m *hbot.Message) bool {
-		roll := strings.SplitAfter(m.Content, " ")
-		rollResult, _, err := dice.Roll(roll[1])
-		if err != nil {
-			log.Error(err.Error())
-			irc.Reply(m, "Usage: [num dice]d[sides](+/-num) (opt: if fudging)")
-			return false // keep processing triggers
+		var resp string
+		cmd := strings.Split(m.Content, " ")
+		if len(cmd) > 1 {
+			resp = roll(cmd[1])
+		} else {
+			resp = DICE_USAGE
 		}
-		if roll[0] != "!roll" {
-			log.Error(err.Error())
-			irc.Reply(m, "You didn't call the command correctly.")
-			return false
-		}
-
-		resp := fmt.Sprintf("%v", rollResult.Int())
 		irc.Reply(m, resp)
 		return false
 	},
+}
+
+// function roll always returns a string to send back to chat
+// and logs an error if one appears
+func roll(r string) string {
+	res, _, err := dice.Roll(r)
+	if err != nil {
+		log.Error(err.Error())
+		return DICE_USAGE
+	}
+	return fmt.Sprintf("%v", res.Int())
 }
