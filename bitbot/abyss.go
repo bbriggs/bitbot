3fc3da5d2dcd15a7f1cdac9b92f50b7e80fa57fe
Bren Briggs <code@fraq.io>
2018-11-17 19:29:25 -0500

    Make abyss trigger way less frequently

--- bitbot/abyss.go
+++ bitbot/abyss.go
@@ -6,7 +6,7 @@ import (
 
 var AbyssTrigger = hbot.Trigger{
 	func(irc *hbot.Bot, m *hbot.Message) bool {
-		return m.Command == "PRIVMSG" && b.Random.Intn(100) < 2
+		return m.Command == "PRIVMSG" && b.Random.Intn(1000) < 2
 	},
 	func(irc *hbot.Bot, m *hbot.Message) bool {
 		irc.Reply(m, "0.0")
