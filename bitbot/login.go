d4c814729ceebb464a0691c4cf25765e42451b62
Bren Briggs <bbriggs@users.noreply.github.com>
2018-11-18 21:23:42 -0500

    Add support for nickserv, server oper, and channel oper (#36)
    * Add new flags for logging into nickserv and oper. Attempt to get +o in channels joined.
    
    * Bump version to 1.0.0
    
    * Replace version subcommand with --version flag

---
+++ bitbot/login.go
@@ -0,0 +1,54 @@
+package bitbot
+
+import (
+	"fmt"
+	"github.com/whyrusleeping/hellabot"
+	"gopkg.in/sorcix/irc.v1"
+	"time"
+)
+
+var OperLogin = hbot.Trigger{
+	func(bot *hbot.Bot, m *hbot.Message) bool {
+		return m.Command == irc.RPL_MYINFO // message type 004
+	},
+
+	func(bot *hbot.Bot, m *hbot.Message) bool {
+		ns, ok := b.NickservLogin()
+		if ok {
+			bot.Msg("NickServ", ns)
+		}
+		time.Sleep(5 * time.Second)
+
+		op, ok := b.OperLogin()
+		if ok {
+			bot.Send(op)
+			time.Sleep(5 * time.Second)
+			b.GetOper()
+		}
+
+		return true
+	},
+}
+
+func (b Bot) OperLogin() (string, bool) {
+	//login := fmt.Sprintf("%+v", b.Config)
+	if b.Config.OperUser == "" || b.Config.OperPass == "" {
+		return "", false
+	}
+	login := fmt.Sprintf("OPER %s %s", b.Config.OperUser, b.Config.OperPass)
+	return login, true
+}
+
+func (b Bot) NickservLogin() (string, bool) {
+	if b.Config.NickservPass == "" {
+		return "", false
+	}
+	login := fmt.Sprintf("IDENTIFY %s", b.Config.NickservPass)
+	return login, true
+}
+
+func (b Bot) GetOper() {
+	for _, channel := range b.Config.Channels {
+		b.Bot.ChMode(b.Bot.Nick, channel, "+o")
+	}
+}
