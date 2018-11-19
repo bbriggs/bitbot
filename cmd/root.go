f23b6a7b6480e6086149c66c21671093e239f35d
Bren Briggs <code@fraq.io>
2018-11-18 21:22:28 -0500

    Replace version subcommand with --version flag

--- cmd/root.go
+++ cmd/root.go
@@ -24,6 +24,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/bbriggs/bitbot/bitbot"
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -33,8 +34,9 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "bitbot",
-	Short: "A brief description of your application",
+	Use:     "bitbot",
+	Short:   "A brief description of your application",
+	Version: fmt.Sprintf("%s (%s/%s)", bitbot.GitVersion, bitbot.GitBranch, bitbot.GitCommit),
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -53,10 +55,6 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.bitbot.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
