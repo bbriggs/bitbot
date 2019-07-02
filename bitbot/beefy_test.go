ff2ac2d47326bf9a5af59a38e82f6c8fa1c2633e
Bren Briggs <code@fraq.io>
2019-07-02 19:17:39 -0400

    BIGGER BEEFIER LETTERS
    Signed-off-by: Bren Briggs <code@fraq.io>

--- bitbot/beefy_test.go
+++ bitbot/beefy_test.go
@@ -31,3 +31,28 @@ func TestBeefDetection(t *testing.T) {
 		t.Errorf("Trigger did not activate. Expected true when given m.Trailing of %s", m.Trailing)
 	}
 }
+
+func TestBigBeefyLetters(t *testing.T) {
+	m := &hbot.Message{
+		&irc.Message{
+			&irc.Prefix{
+				"",
+				"",
+				"",
+			},
+			"PRIVMSG",
+			[]string{},
+			":BEEFY",
+			true,
+		},
+		"",
+		time.Now(),
+		"",
+		"",
+	}
+	b := &hbot.Bot{}
+	ok := BeefyTrigger.Condition(b, m)
+	if !ok {
+		t.Errorf("Trigger did not activate. Expected true when given m.Trailing of %s", m.Trailing)
+	}
+}
