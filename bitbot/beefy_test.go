5aa3b9b688f0d2b07c4af700a5c15862d4e4b50f
Bren Briggs <code@fraq.io>
2019-07-02 19:14:56 -0400

    BEEF UP THE TESTS
    Signed-off-by: Bren Briggs <code@fraq.io>

---
+++ bitbot/beefy_test.go
@@ -0,0 +1,33 @@
+package bitbot
+
+import (
+	"github.com/whyrusleeping/hellabot"
+	"gopkg.in/sorcix/irc.v1"
+	"testing"
+	"time"
+)
+
+func TestBeefDetection(t *testing.T) {
+	m := &hbot.Message{
+		&irc.Message{
+			&irc.Prefix{
+				"",
+				"",
+				"",
+			},
+			"PRIVMSG",
+			[]string{},
+			":beefy",
+			true,
+		},
+		"",
+		time.Now(),
+		"",
+		"",
+	}
+	b := &hbot.Bot{}
+	ok := BeefyTrigger.Condition(b, m)
+	if !ok {
+		t.Errorf("Trigger did not activate. Expected true when given m.Trailing of %s", m.Trailing)
+	}
+}
