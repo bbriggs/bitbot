87532e49ce2917429cba0bd2c962b5c85378d6b0
Bren Briggs <code@fraq.io>
2019-07-03 19:32:05 -0400

    Use regular random instead of reaching out to global bot object
    Signed-off-by: Bren Briggs <code@fraq.io>

--- bitbot/abyss.go
+++ bitbot/abyss.go
@@ -2,12 +2,13 @@ package bitbot
 
 import (
 	"github.com/whyrusleeping/hellabot"
+	"math/rand"
 )
 
 var AbyssTrigger = NamedTrigger{
 	ID: "abyss",
 	Condition: func(irc *hbot.Bot, m *hbot.Message) bool {
-		return m.Command == "PRIVMSG" && b.Random.Intn(1000) < 2
+		return m.Command == "PRIVMSG" && rand.Intn(1000) < 2
 	},
 	Action: func(irc *hbot.Bot, m *hbot.Message) bool {
 		irc.Reply(m, "0.0")
