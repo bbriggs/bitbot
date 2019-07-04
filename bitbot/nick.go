2231440cd6660edf2b99363152d921482f73005c
m-242 <m242@protonmail.com>
2019-07-04 16:53:49 +0200

    added a nick-changing trigger

---
+++ bitbot/nick.go
@@ -0,0 +1,24 @@
+package bitbot
+
+import (
+	"strings"
+
+	"github.com/whyrusleeping/hellabot"
+)
+
+var NickTrigger = NamedTrigger{
+	ID: "nick",
+	Condition: func(irc *hbot.Bot, m *hbot.Message) bool {
+		var comesFromHost = (m.From == strings.Split(irc.Host, ":")[0])
+
+		var nickTaken = strings.Contains(m.Content,
+			"Nickname is already in use")
+
+		return comesFromHost && nickTaken
+	},
+	Action: func(irc *hbot.Bot, m *hbot.Message) bool {
+		nick := irc.Nick + "_"
+		irc.SetNick(nick)
+		return true
+	},
+}
