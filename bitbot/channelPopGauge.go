d17c7c8e1717df32cc0d9a544e3f4fd728ca59c8
Bren Briggs <code@fraq.io>
2019-10-16 00:17:45 -0400

    Add new trigger to watch for RPL_LIST messages from server. Fixes #76
    Signed-off-by: Bren Briggs <code@fraq.io>

--- bitbot/channelPopGauge.go
+++ bitbot/channelPopGauge.go
@@ -1,25 +1,51 @@
 package bitbot
 
 import (
+	"log"
+	"strconv"
+	"strings"
+
 	"github.com/whyrusleeping/hellabot"
 	"gopkg.in/sorcix/irc.v1"
-	//"log"
-	"strings"
 )
 
 var ChannelPopGaugeTrigger = NamedTrigger{
 	ID:   "channelPopGauge",
 	Help: "Updates a Prometheus gauge with the value of a channel's population",
 	Condition: func(bot *hbot.Bot, m *hbot.Message) bool {
-		return m.Command == "JOIN" || m.Command == "PART" || m.Command == "QUIT" || m.Command == "SQUIT"
+		return m.Command == "JOIN" || m.Command == "PART" || m.Command == "QUIT"
 
 	},
 	Action: func(bot *hbot.Bot, m *hbot.Message) bool {
-		if m.Command == "JOIN" {
+		switch m.Command {
+		case "JOIN":
 			b.gauges["channel_pop"].WithLabelValues(m.To).Inc()
-			return true
+		case "QUIT":
+			for _, c := range b.Bot.Channels {
+				bot.Send("LIST " + c)
+			}
+		default:
+			b.gauges["channel_pop"].WithLabelValues(m.To).Dec()
 		}
-		b.gauges["channel_pop"].WithLabelValues(m.To).Dec()
+
+		return true
+	},
+}
+
+var HandleListReplyTrigger = NamedTrigger{
+	ID:   "handleListReply",
+	Help: "Sets the gauge for a channel pop when an RPL_LIST command is detected",
+	Condition: func(bot *hbot.Bot, m *hbot.Message) bool {
+		return m.Command == irc.RPL_LIST
+	},
+	Action: func(bot *hbot.Bot, m *hbot.Message) bool {
+		log.Println("List Reply Received")
+		channel := m.Params[1]
+		pop, err := strconv.Atoi(m.Params[2])
+		if err != nil {
+			log.Println("RPL_LIST returned invalid reply")
+		}
+		b.gauges["channel_pop"].WithLabelValues(channel).Set(float64(pop))
 		return true
 	},
 }
@@ -34,7 +60,7 @@ var SetChanPopGaugeTrigger = NamedTrigger{
 		names := strings.Split(strings.TrimSpace(m.Content), " ")
 		channel := m.Params[len(m.Params)-1]
 		// log.Printf("Setting channel_pop to %d\n\n", len(names))
-		b.gauges["channel_pop"].WithLabelValues(channel).Set(float64(len(names))) // Exclude ourselves. ChanPopGaugeTrigger catches our own join.
+		b.gauges["channel_pop"].WithLabelValues(channel).Set(float64(len(names)))
 		return true
 	},
 }
