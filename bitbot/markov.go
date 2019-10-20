69fa258519d7ccedb5dfd4307f98a86bf427feed
Seth Beckman <beckmanseth@gmail.com>
2019-10-20 12:50:37 -0400

    Markov Init/Reset
    Added a trigger for `!markov` which takes params `reset` or `init` to either clear the markov chain and start a new one, or initialize with hardcoded sources, respectively. Also created a wrapper, `markovAdd()` for locking, adding, and unlocking mutex.

--- bitbot/markov.go
+++ bitbot/markov.go
@@ -1,6 +1,8 @@
 package bitbot
 
 import (
+	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"strings"
 	"net/http"
@@ -20,7 +22,7 @@ var MarkovTrainerTrigger = NamedTrigger{
 			// Initialize markov chain
 			b.mChain = gomarkov.NewChain(1)
 		}
-		markovAdd(b.mChain)
+		markovAdd(m.Content, b.mChain)
 		return false
 	},
 }
@@ -41,9 +43,9 @@ var MarkovInitTrigger = NamedTrigger{
 	ID: "markovInit",
 	Help: "Resets markov chain to a fresh chain, or bootstraps it with sample texts. Usage: !markov reset, !markov init",
 	Condition: func(irc *hbot.Bot, m *hbot.Message) bool {
-		return m.Command == "PRIVMSG" && strings.HasPrefix(m.Content == "!markov ")
+		return m.Command == "PRIVMSG" && strings.HasPrefix(m.Content, "!markov ")
 	},
-	Action: func(irc *hbot.bot, m *hbot.Message) bool {
+	Action: func(irc *hbot.Bot, m *hbot.Message) bool {
 		cmd := strings.Split(m.Content, " ")
 		if len(cmd) < 2 {
 			return false
@@ -56,13 +58,12 @@ var MarkovInitTrigger = NamedTrigger{
 		case "init":
 			// do other stuff
 			b.mChain = gomarkov.NewChain(1)
-			markovInit(b.mChain)
-			return true
+			return markovInit(b.mChain)
 		default:
 			// didn't recognize the subcommands
 			return false
 		}
-	}
+	},
 	
 }
 
@@ -75,8 +76,24 @@ func generateBabble(chain *gomarkov.Chain) string {
 	return strings.Join(tokens[1:len(tokens)-1], " ")
 }
 
-func markovInit(chain *gomarkov.Chain) string {
-	//todo
+func markovInit(chain *gomarkov.Chain) bool {
+	var sources = [3]string{"https://gist.githubusercontent.com/bbriggs/60e907f3571a1ca7c41cd99f78052d78/raw/fe6d0bd96ee97c9b5df2794ae683d24a404b4433/bible.txt",
+		"https://gist.githubusercontent.com/bbriggs/f63340a3ed1a1439b6f3f8d619eacac1/raw/1f363d500226c55bab735fe59074f06721348546/world_factbook.txt",
+		"https://gist.githubusercontent.com/parsec/2f4d4edf55336c0a2994cfcf951a8ea7/raw/4b66c99f1879b927ebc2b2ffb8fdd39dc9a4f7d2/SnwCrsh"}
+
+	i := 0
+	for i <= len(sources) {
+		resp, err := http.Get(sources[i])
+		if err != nil {
+			fmt.Println(err)
+			return false
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		bodyString := string(body)
+		markovAdd(bodyString, chain)
+	}
+	return true
 }
 
 // wrapper for b.mChain.Add that includes file lock/unlock
@@ -84,4 +101,5 @@ func markovAdd(text string, chain *gomarkov.Chain) {
 	b.markovMutex.Lock()
 	b.mChain.Add(strings.Split(text, " "))
 	b.markovMutex.Unlock()
+
 }
