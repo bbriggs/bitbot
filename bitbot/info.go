4292cf75842b43d860a80f2f98a9d52882741ff2
Bren Briggs <code@fraq.io>
2019-11-15 22:46:08 -0500

    Properly set version/commit/branch info at build time
    Signed-off-by: Bren Briggs <code@fraq.io>

--- bitbot/info.go
+++ bitbot/info.go
@@ -13,7 +13,7 @@ var InfoTrigger = NamedTrigger{
 		return m.Command == "PRIVMSG" && strings.TrimSpace(m.Content) == "!info"
 	},
 	Action: func(irc *hbot.Bot, m *hbot.Message) bool {
-		resp := fmt.Sprintf("Bitbot version %s (%s/%s) | %s", GitVersion, GitBranch, GitCommit, SourceRepo)
+		resp := fmt.Sprintf("Bitbot version %s (%s/%s) | %s", GitTag, GitBranch, GitCommit, SourceRepo)
 		irc.Reply(m, resp)
 		return true
 	},
