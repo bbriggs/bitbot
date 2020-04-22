ae3accc4de32714a2bb2539ff225e96fbfb2e6f6
Olivier Moreau m242 <m242@protonmail.com>
2020-04-22 12:58:05 +0200

    Bugs correction and code formatting.
    The author of an event had the ability to leave it, added a check.
    The `!remind time` command didn't return UTC time.
    
    Signed-off-by: Olivier Moreau m242 <m242@protonmail.com>

--- bitbot/reminder.go
+++ bitbot/reminder.go
@@ -27,6 +27,15 @@ type ReminderEvent struct {
 	Time        time.Time
 }
 
+// This error is used when a badly formatted call of the trigger is made.
+type wrongFormatError struct {
+	arg string
+}
+
+func (e *wrongFormatError) Error() string {
+	return fmt.Sprintf("%s : is not of the awaited format", e.arg)
+}
+
 var ReminderTrigger = NamedTrigger{ //nolint:gochecknoglobals,golint
 	ID:   "reminder",
 	Help: "Set up events and remind them to concerned people. Usage: !remind list|time|add|remove|join|part",
@@ -74,155 +83,9 @@ var ReminderTrigger = NamedTrigger{ //nolint:gochecknoglobals,golint
 	},
 }
 
-// This error is used when a badly formatted call of the trigger is made.
-type wrongFormatError struct {
-	arg string
-}
-
-func (e *wrongFormatError) Error() string {
-	return fmt.Sprintf("%s : is not of the awaited format", e.arg)
-}
-
-func getMessageIDFromString(body string) (int, error) {
-	// Parse message
-	msg := strings.Split(body, " ")
-	isAnID, err := regexp.MatchString("[0-9]+", msg[2])
-	if err != nil {
-		log.Info("Not and ID :", err)
-	}
-
-	if len(msg) != 3 || !isAnID {
-		return -1, &wrongFormatError{body}
-	}
-	return strconv.Atoi(msg[2])
-}
-
-// Signal yourself as interested in an event (Facebook™)
-func joinEvent(message *hbot.Message) string {
-	var event ReminderEvent
-
-	id, err := getMessageIDFromString(message.Content)
-	if err != nil {
-		return "Wrong command. format is : !remind join [ID]"
-	}
-
-	b.DB.Where("ID = ?", id).Take(&event)
-
-	if strings.Contains(event.People, message.Name) {
-		b.DB.Save(&event)
-		return "You already subscribed to this event"
-	}
-	event.People = fmt.Sprintf("%s%s ", event.People, message.Name)
-	b.DB.Save(&event)
-
-	feedback := fmt.Sprintf("Added %s to \"%s\"",
-		message.Name,
-		event.Description)
-
-	return feedback
-}
-
-func partEvent(message *hbot.Message) string {
-	var event ReminderEvent
-
-	id, err := getMessageIDFromString(message.Content)
-	if err != nil {
-		return "Wrong command. format is : !remind part [ID]"
-	}
-
-	b.DB.Where("ID = ?", id).Take(&event)
-
-	event.People = strings.Replace(event.People, message.Name+" ", "", -1)
-	b.DB.Save(&event)
-
-	feedback := fmt.Sprintf("Removed %s from \"%s\"",
-		message.Name,
-		event.Description)
-
-	return feedback
-}
-
-// Remove an event given by his ID
-func removeEvent(message *hbot.Message) string {
-	var event ReminderEvent
-
-	id, err := getMessageIDFromString(message.Content)
-	if err != nil {
-		return "Wrong command. format is : !remind remove [ID]"
-	}
-
-	b.DB.Where("ID = ? AND Author = ?", id, message.Name).Take(&event)
-
-	// Feedback Message construction
-	var feedbackMessage string
-	if event.ID == id {
-		feedbackMessage = fmt.Sprintf("Deleted event %d : %s",
-			event.ID,
-			event.Description)
-
-		// Delete
-		b.DB.Delete(&event)
-	} else {
-		feedbackMessage = "No event you own with that ID"
-	}
-
-	return feedbackMessage
-}
-
-// Lists all the awaiting events in PM
-func listEvents(message *hbot.Message, bot *hbot.Bot) string {
-	// Get all the db rows, iterate through them, format them and send them to pm
-	rows, err := b.DB.Model(&ReminderEvent{}).Rows()
-	if err != nil {
-		log.Error("Reminder: Couldn't get DB rows", err)
-	}
-
-	var (
-		event                   ReminderEvent
-		eventDescriptionMessage string
-	)
-
-	for rows.Next() {
-		err := b.DB.ScanRows(rows, &event)
-		if err != nil {
-			log.Error("Reminder: Couldn't get a db row", err)
-		}
-
-		eventDescriptionMessage = fmt.Sprintf(
-			"%d : [ %s ] at %s. Event author : %s, in channel %s, with %s",
-			event.ID,
-			event.Description,
-			event.Time.Format(timeFormat),
-			event.Author,
-			event.Channel,
-			event.People)
-		bot.Msg(message.Name, eventDescriptionMessage)
-	}
-
-	return "I've PM'd you the list of awaiting events"
-}
-
-func parseAddCommandMessage(body string) (string, string, error) {
-	var timeOfEvent string
-
-	messageSplit := strings.SplitAfterN(body, " ", 3)
-	if len(messageSplit) <= 2 {
-		return "", "", &wrongFormatError{body}
-	}
-	body = messageSplit[2]
-
-	timeOfEventSliced := strings.Split(body, " ")
-	if len(timeOfEventSliced) > 2 {
-		timeOfEvent = strings.Join(
-			timeOfEventSliced[len(timeOfEventSliced)-2:],
-			" ")
-	} else {
-		return "", "", &wrongFormatError{body}
-	}
-
-	description := strings.Replace(body, timeOfEvent, "", -1)
-
-	return description, timeOfEvent, nil
+// Get the time in UTC, formatted in the expected way.
+func getTime() string {
+	return time.Now().In(location).Format(timeFormat)
 }
 
 // Parses an event adding message and adds the event
@@ -286,6 +149,148 @@ func addEvent(message *hbot.Message, bot *hbot.Bot) string {
 		channel)
 }
 
-func getTime() string {
-	return time.Now().Format(timeFormat)
+// Remove an event given by his ID
+func removeEvent(message *hbot.Message) string {
+	var event ReminderEvent
+
+	id, err := getMessageIDFromString(message.Content)
+	if err != nil {
+		return "Wrong command. format is : !remind remove [ID]"
+	}
+
+	b.DB.Where("ID = ? AND Author = ?", id, message.Name).Take(&event)
+
+	// Feedback Message construction
+	var feedbackMessage string
+	if event.ID == id {
+		feedbackMessage = fmt.Sprintf("Deleted event %d : %s",
+			event.ID,
+			event.Description)
+
+		// Delete
+		b.DB.Delete(&event)
+	} else {
+		feedbackMessage = "No event you own with that ID"
+	}
+
+	return feedbackMessage
+}
+
+// Lists all the awaiting events in PM
+func listEvents(message *hbot.Message, bot *hbot.Bot) string {
+	// Get all the db rows, iterate through them, format them and send them to pm
+	rows, err := b.DB.Model(&ReminderEvent{}).Rows()
+	if err != nil {
+		log.Error("Reminder: Couldn't get DB rows", err)
+	}
+
+	var (
+		event                   ReminderEvent
+		eventDescriptionMessage string
+	)
+
+	for rows.Next() {
+		err := b.DB.ScanRows(rows, &event)
+		if err != nil {
+			log.Error("Reminder: Couldn't get a db row", err)
+		}
+
+		eventDescriptionMessage = fmt.Sprintf(
+			"%d : [ %s ] at %s. Event author : %s, in channel %s, with %s",
+			event.ID,
+			event.Description,
+			event.Time.Format(timeFormat),
+			event.Author,
+			event.Channel,
+			event.People)
+		bot.Msg(message.Name, eventDescriptionMessage)
+	}
+
+	return "I've PM'd you the list of awaiting events"
+}
+
+// Signal yourself as interested in an event (Facebook™)
+func joinEvent(message *hbot.Message) string {
+	var event ReminderEvent
+
+	id, err := getMessageIDFromString(message.Content)
+	if err != nil {
+		return "Wrong command. format is : !remind join [ID]"
+	}
+
+	b.DB.Where("ID = ?", id).Take(&event)
+
+	if strings.Contains(event.People, message.Name) {
+		b.DB.Save(&event)
+		return "You already subscribed to this event"
+	}
+	event.People = fmt.Sprintf("%s%s ", event.People, message.Name)
+	b.DB.Save(&event)
+
+	feedback := fmt.Sprintf("Added %s to \"%s\"",
+		message.Name,
+		event.Description)
+
+	return feedback
+}
+
+func partEvent(message *hbot.Message) string {
+	var event ReminderEvent
+
+	id, err := getMessageIDFromString(message.Content)
+	if err != nil {
+		return "Wrong command. format is : !remind part [ID]"
+	}
+
+	b.DB.Where("ID = ?", id).Take(&event)
+	defer b.DB.Save(&event)
+
+	if (event.Author == message.Name) {
+		return "Author of an event can't leave it."
+	}
+	
+	event.People = strings.Replace(event.People, message.Name+" ", "", -1)
+	
+	feedback := fmt.Sprintf("Removed %s from \"%s\"",
+		message.Name,
+		event.Description)
+
+	return feedback
+}
+
+func parseAddCommandMessage(body string) (string, string, error) {
+	var timeOfEvent string
+
+	messageSplit := strings.SplitAfterN(body, " ", 3)
+	if len(messageSplit) <= 2 {
+		return "", "", &wrongFormatError{body}
+	}
+	body = messageSplit[2]
+
+	timeOfEventSliced := strings.Split(body, " ")
+	if len(timeOfEventSliced) > 2 {
+		timeOfEvent = strings.Join(
+			timeOfEventSliced[len(timeOfEventSliced)-2:],
+			" ")
+	} else {
+		return "", "", &wrongFormatError{body}
+	}
+
+	description := strings.Replace(body, timeOfEvent, "", -1)
+
+	return description, timeOfEvent, nil
+}
+
+func getMessageIDFromString(body string) (int, error) {
+	// Parse message
+	msg := strings.Split(body, " ")
+	isAnID, err := regexp.MatchString("[0-9]+", msg[2])
+	if err != nil {
+		log.Info("Not and ID :", err)
+	}
+
+	if len(msg) != 3 || !isAnID {
+		return -1, &wrongFormatError{body}
+	}
+	return strconv.Atoi(msg[2])
 }
