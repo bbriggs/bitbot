fc366c6564e769f3185ba374184772eb72c5e0fe
m242 <m242@protonmail.com>
2020-05-23 18:38:28 +0200

    Made epeen trigger easier to test

--- bitbot/epeen.go
+++ bitbot/epeen.go
@@ -13,22 +13,25 @@ var EpeenTrigger = NamedTrigger{ //nolint:gochecknoglobals,golint
 		return m.Command == "PRIVMSG" && strings.TrimSpace(m.Content) == "!epeen"
 	},
 	Action: func(irc *hbot.Bot, m *hbot.Message) bool {
-		var nick = m.From
-		var peepee = ""
-
-		switch nick {
-		case "daemon":
-			peepee = strings.Repeat("\\", rand.Intn(200)) + "D" + "\n(_)_)"
-		case "m242":
-			peepee = "8D" // Yup, childish :D
-		case "skidd0":
-			peepee = "8=ancap=D"
-		default:
-			peepee = "8" + strings.Repeat("=", rand.Intn(20)) + "D"
-		}
-		var reply = nick + "'s peepee: " + peepee
-
-		irc.Reply(m, reply)
+		var epeen = makeEpeenAnswer(m.From)
+		irc.Reply(m, epeen)
 		return true
 	},
+}
+
+func makeEpeenAnswer(nick string) string {
+	var peepee = ""
+
+	switch nick {
+	case "daemon":
+		peepee = strings.Repeat("\\", rand.Intn(200)) + "D" + "\n(_)_)"
+	case "m242":
+		peepee = "8D" // Yup, childish :D
+	case "skidd0":
+		peepee = "8=ancap=D"
+	default:
+		peepee = "8" + strings.Repeat("=", rand.Intn(20)) + "D"
+	}
+	return nick + "'s peepee: " + peepee
+
 }
