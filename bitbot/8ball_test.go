4859f1c9b4be3293706f7bb9803e6a8a2e52a30c
m242 <m242@protonmail.com>
2020-05-24 10:56:05 +0200

    Testing 8ball trigger

---
+++ bitbot/8ball_test.go
@@ -0,0 +1,26 @@
+package bitbot
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func TestMake8BallAnswer(t *testing.T) {
+	rand.Seed(1)
+	var testAnswers string
+
+	answersMap := make(map[string]int)
+	for _, ans := range magic8responses {
+		answersMap[ans] = 0
+	}
+
+	// We get 200 random answers, and test that every one of them is awaited
+	for i := 1; i <= 200; i++ {
+		testAnswers = make8BallAnswer()
+
+		// It's one of the allowed answers
+		if _, ok := answersMap[testAnswers]; !ok {
+			t.Errorf("Wrong answer for 8ball : %s", testAnswers)
+		}
+	}
+}
