62121f72dd6397b9e22cf7c366ecc0813b760015
Bren Briggs <code@fraq.io>
2020-11-18 14:55:54 -0500

    Insert nicks to database

--- bitbot/nickShuffle.go
+++ bitbot/nickShuffle.go
@@ -17,44 +17,48 @@ var (
 
 // ReminderEvent : The Gorm struct that represents an event in the DB.
 type NickList struct {
-	ID   int    `gorm:"unique;AUTO_INCREMENT;PRIMARY_KEY"`
-	Nick string `gorm:"unique"`
+	ID   int    `gorm:"unique;AUTO_INCREMENT;PRIMARY_KEY"` // Primary Key
+	Nick string `gorm:"unique"`                            // Nickname to use
+	From string // Submitter of the nickname
 }
 
 var NickShuffleTrigger = NamedTrigger{ //nolint:gochecknoglobals,golint
 	ID:   "nickShuffle",
-	Help: "Add a nick to my shuffle. Usage: !nick add|drop <nick>",
+	Help: "Add a nick to my shuffle. Usage: !nick add <nick>",
 	Init: func() error {
 		return b.DB.AutoMigrate(&NickList{}).Error
 	},
 	Condition: func(irc *hbot.Bot, m *hbot.Message) bool {
-		return m.Command == "PRIVMSG" && strings.HasPrefix(m.Trailing, "!remind")
+		return m.Command == "PRIVMSG" && strings.HasPrefix(m.Trailing, "!nick add <nickname>")
 	},
 	Action: func(irc *hbot.Bot, m *hbot.Message) bool {
-		timeFormat = "2006-01-02 15:04"
-
-		splitMSG := strings.Split(m.Content, " ")
-		if len(splitMSG) < 2 {
-			irc.Reply(m, "Not enough arguments provided")
-			return true
-		}
-
-		switch splitMSG[1] {
-		case "time":
-			irc.Reply(m, getTime())
-		case "add":
-			irc.Reply(m, addEvent(m, irc))
-		case "remove":
-			irc.Reply(m, removeEvent(m))
-		case "list":
-			irc.Reply(m, listEvents(m, irc))
-		case "join":
-			irc.Reply(m, joinEvent(m))
-		case "part":
-			irc.Reply(m, partEvent(m))
-		default:
-			irc.Reply(m, "Wrong argument")
+		ok, err := addNickToDB(m)
+		if !ok {
+			irc.Reply("Looks like something went wrong.")
+			irc.Reply(err.Error())
 		}
 		return true
 	},
+}
+
+func addNickToDB(m *hbot.Message) (bool, error) {
+	// split message, error out if too short
+	splitMsg := strings.Split(m.Content, " ")
+	if len(splitMsg) < 3 {
+		return false, fmt.Errorf("Not enough arguments. See !help nickShuffle")
+	}
+
+	// grab nick, error out if invalid
+	newNick := NickList{
+		Nick: splitMsg[2],
+		From: m.From,
+	}
+
+	// insert into database
+	b.DB.NewRecord(newNick)
+	if err := b.DB.Create(&newNick); err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
