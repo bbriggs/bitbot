6f8aefe09b9d3d87b075a771c08549140b34fbeb
MOREAU Olivier <m242@pm.me>
2021-02-25 19:20:57 +0100

    why be clever

--- bitbot/reactions.go
+++ bitbot/reactions.go
@@ -1,8 +1,8 @@
 package bitbot
 
 import (
+	"regexp"
 	"strings"
-	"unicode"
 
 	"github.com/whyrusleeping/hellabot"
 )
@@ -35,8 +35,8 @@ var WeebTrigger = NamedTrigger{ //nolint:gochecknoglobals,golint
 	ID:   "DamnWeebs",
 	Help: "Usage: mention uwu",
 	Condition: func(irc *hbot.Bot, m *hbot.Message) bool {
-		// Thanks d4 for the help!
-		return m.Command == "PRIVMSG" && containsOwOLike(m.Content)
+		match, _ := regexp.MatchString(`(?i) uwu | owo |^owo$|^uwu$`, m.Content)
+		return m.Command == "PRIVMSG" && match
 	},
 	Action: func(irc *hbot.Bot, m *hbot.Message) bool {
 		adjectives := []string{ //add more as necessary
@@ -53,44 +53,4 @@ var WeebTrigger = NamedTrigger{ //nolint:gochecknoglobals,golint
 		irc.Reply(m, reply)
 		return false
 	},
-}
-
-// containsOwOLike returns true if the message argument contains owo, uWu, 0w0,
-// or any other similarly repeating a character around a w.
-// We could have done that with a PCRE regex, but golang uses re2[0] and I'm not
-// adding a dependency wrapping PCRE simply to shame weebs, furries, and similar
-// creatures.
-// [0] https://github.com/google/re2
-// Go suxx, brainfuck rewrite when
-func containsOwOLike(message string) bool {
-	words := strings.Split(message, " ")
-	for _, w := range words {
-		switch len(w) {
-		case 1, 2: //nolint:gomnd
-		case 3: //nolint:gomnd
-			if (w[0] == w[2]) && (w[1] == 87 || w[1] == 119) {
-				return true
-			}
-		default:
-			if owoInWord(w) {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func owoInWord(word string) bool { // The word contains a [^A-Z]W[^A-Z] or a [A-Z]w[A-Z]
-	ws := strings.Split(word, "")
-	for x := 1; x < len(word)-1; x++ {
-		if word[x] > unicode.MaxASCII {
-			return false
-		}
-		if ws[x] == "w" && word[x-1] > 64 && word[x-1] < 88 && ws[x-1] == ws[x+1] {
-			return true
-		} else if ws[x] == "W" && (word[x-1] < 65 || word[x-1] > 87) {
-			return true
-		}
-	}
-	return false
 }
